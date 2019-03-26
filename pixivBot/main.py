be99050d2d9a60545a687e40b8b40d29f84f72d8
Shimada666 <649940882@qq.com>
2019-03-26 13:10:14 +0800

    数据集从此诞生

--- pixivBot/main.py
+++ pixivBot/main.py
@@ -7,11 +7,11 @@ import threading
 import json
 import sys
 from random import randint
-import uuid
+from uuid import uuid4
 
 
 # 热门榜
-def rank_by_time(mode='day_male', num=1):    # (mode, date = None, offset = None) 作品排行
+def rank_by_time(api, mode='day_male', num=1):    # (mode, date = None, offset = None) 作品排行
     json_result = api.illust_ranking(mode=mode)
     # print(json_result)
     img_list_out = []
@@ -24,7 +24,7 @@ def rank_by_time(mode='day_male', num=1):    # (mode, date = None, offset = None
 
                 # api.download(illust.image_urls['large'], path='img/', name='popular_' + str(i) + '.jpg')
                 # address = os.getcwd() + '\\img\\popular_' + str(i) + '.jpg'
-                uuid_name = uuid.uuid4().hex
+                uuid_name = uuid4().hex
                 api.download(illust.image_urls['large'], path='img/', name=uuid_name + '.jpg')
                 address = os.getcwd() + '\\img\\' + uuid_name + '.jpg'
                 img_list_out.append(address)
@@ -39,7 +39,7 @@ def rank_by_time(mode='day_male', num=1):    # (mode, date = None, offset = None
 
 
 # 按tag搜索最热作品（按时间排序） 非会员使用
-def search_by_tag_ranked(word, search_target='partial_match_for_tags', sort='date_desc', duration=None, p_num=15):
+def search_by_tag_ranked(word, api, search_target='partial_match_for_tags', sort='date_desc', duration=None, p_num=15):
     img_list = []
     booked_list = []
     for pi in range(p_num):
@@ -61,7 +61,7 @@ def search_by_tag_ranked(word, search_target='partial_match_for_tags', sort='dat
 
 
 # tag热图搜索 会员限定
-def search_by_tag_popular(word, duration=None, num=1, p_num=2):
+def search_by_tag_popular(word, api, duration=None, num=1, p_num=2):
     img_list = []
     img_list_out = []
     for pi in range(p_num):
@@ -72,7 +72,7 @@ def search_by_tag_popular(word, duration=None, num=1, p_num=2):
             i = r.randint(0, len(img_list) - 1)
             illust = img_list[i]
             word = word.replace(" ", "_")
-            uuid_name = uuid.uuid4().hex
+            uuid_name = uuid4().hex
             api.download(illust.image_urls['large'], path='img/', name=word + '_' + uuid_name + '.jpg')
             img_list_out.append(os.getcwd() + '\\img\\' + word + '_' + uuid_name + '.jpg')
         return img_list_out, ''
@@ -81,7 +81,7 @@ def search_by_tag_popular(word, duration=None, num=1, p_num=2):
 
 
 # 获取tag列表
-def get_tags():
+def get_tags(api):
     tags = api.trending_tags_illust()
     i = 0
     tags_list = []
@@ -112,6 +112,12 @@ def time_limit(interval):
 
 
 def main(text):
+    _USERNAME = "corgiclub@yeah.net"
+    _PASSWORD = "corgiclubADMIN"
+
+    # 登录api
+    api = p.AppPixivAPI()
+    api.login(_USERNAME, _PASSWORD)
 
     # 获得输入 可接受输入：来份色图/来n份色图/来份图/来n份图/tag... 其他输入会输出'你在做什么我不太懂呀~'
     # with open("pixivBot/out.txt", "r", encoding="UTF-8") as f:
@@ -133,7 +139,7 @@ def main(text):
         # time_limit(1)
 
         # 寻找当天r18色图
-        donot_rush = ["别冲了", "营养跟得上吗?", "你撸多了", "别撸了,伤身体", "再冲会暴毙信不", "求求你别冲了", "这么多你营养跟得上?", "求求你别冲了", "冲这么多你今晚biss", "天天看色图不去读书?", "再冲jb都给你剁了", "朋友少撸管多学习"]
+        donot_rush = ["别冲了", "营养跟得上吗?", "你撸多了", "别撸了,伤身体", "再冲会暴毙信不", "求求你别冲了", "冲这么多你营养跟得上?", "求求你别冲了", "冲这么多你今晚biss", "天天看色图不去读书?", "再冲jb都给你剁了", "朋友少撸管多学习"]
         img_lst = []
         if text.startswith('来') and text.endswith('色图'):
             if text.endswith('三次元色图'):
@@ -154,28 +160,28 @@ def main(text):
                     output['img_list'] = img_lst
 
             elif text == '来份色图':
-                output['img_list'], output['text'] = rank_by_time(mode="week_r18")
+                output['img_list'], output['text'] = rank_by_time(api, mode="day_r18")
             else:
                 num = re.search('\d+', text).group()
                 if int(num) >= 10:
                     output["text"] = donot_rush[randint(0, len(donot_rush) - 1)]
                 else:
                     if num:
-                        output['img_list'], output['text'] = rank_by_time(mode="week_r18", num=int(num))
+                        output['img_list'], output['text'] = rank_by_time(api, mode="day_r18", num=int(num))
                     else:
                         output['text'] = '输入有误！'
 
         # 当天热门榜随机
         elif text.startswith('来') and text.endswith('份图'):
             if text == '来份图':
-                output['img_list'], output['text'] = rank_by_time(mode='day_male')
+                output['img_list'], output['text'] = rank_by_time(api, mode='day_male')
             else:
                 num = re.search('\d+', text).group()
                 if int(num) >= 10:
                     output["text"] = "图太多遭不住啊老哥"
                 else:
                     if num:
-                        output['img_list'], output['text'] = rank_by_time(mode='day_male', num=int(num))
+                        output['img_list'], output['text'] = rank_by_time(api, mode='day_male', num=int(num))
                     else:
                         output['text'] = '输入有误！'
         elif text.endswith("kkp") or text.endswith("看看批"):
@@ -185,7 +191,7 @@ def main(text):
                 output['text'] = "看你妈看"
             else:
                 for root, dirs, files in os.walk(url):
-                    img_url = "G:\kkp\\" + files[randint(0, files.__len__() - 1)]
+                    img_url = "G:\\kkp\\" + files[randint(0, files.__len__() - 1)]
                     img_lst.append(img_url)
                 output['img_list'] = img_lst
         # 按tag搜寻:
@@ -195,23 +201,23 @@ def main(text):
                 output['text'] += '用法：\n1 “tag 标签1 标签2 ……”\n2 tag查询后按序号检索\n3 tag? 代表随机一个tag'
             # 列出推荐tag列表
             elif text == 'tag查询':
-                tags_list = get_tags()
+                tags_list = get_tags(api)
                 for i in range(len(tags_list)):
                     output['text'] += str(i + 1) + ' ' + tags_list[i] + '\n'
 
             elif text == 'tag?' or text == 'tag？':
-                output['img_list'], output['text'] = search_by_tag_popular(word=tags_list[randint(0, 39)])
+                output['img_list'], output['text'] = search_by_tag_popular(api=api, word=tags_list[randint(0, 39)])
             # 直接搜寻tag
             elif text.startswith('tag '):
                 word = text[4:]
-                output['img_list'], output['text'] = search_by_tag_popular(word=word)
+                output['img_list'], output['text'] = search_by_tag_popular(api=api, word=word)
         # 按tag列表索引
             else:
                 if not tags_list:
                     output['text'] = '你还没查询过tag，不能使用tag索引！使用"tag查询"来获取tag'
                 else:
                     # print(int(text[3:]) - 1)
-                    output['img_list'], output['text'] = search_by_tag_popular(word=tags_list[int(text[3:]) - 1])
+                    output['img_list'], output['text'] = search_by_tag_popular(api=api, word=tags_list[int(text[3:]) - 1])
 
         elif text == '帮助':
             output['text'] = '可接受指令：\n来(n)份色图\n来(n)份图\n来(n)份三次元色图\ntag'
@@ -239,11 +245,6 @@ def main(text):
 # 全局变量
 # _tags_list = []                         # tag列表
 # 账号密码
-_USERNAME = "corgiclub@yeah.net"
-_PASSWORD = "corgiclubADMIN"
 
-# 登录api
-api = p.AppPixivAPI()
-api.login(_USERNAME, _PASSWORD)
 
 # main()
--- run.py
+++ run.py
@@ -2,13 +2,32 @@ from flask import Flask, request, make_response
 from httpapi.HTTPSDK import *
 from pixivBot.main import main
 from random import randint
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from uuid import uuid4
+import os
 import pixivpy3 as p
 import random as r
+import re
+import requests
 
 # 配置路由，在插件提交返回中配置地址（如本例 http://127.0.0.1:5000）
 # Create your views here.
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(10))
+    text = db.Column(db.Text)
+    path = db.Column(db.String(100))
+    qq = db.Column(db.String(15))
+    group = db.Column(db.String(15))
+    timestamp = db.Column(db.DateTime, default=datetime.now, index=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,11 +36,40 @@ def index():
     sdk = HTTPSDK.httpGet(req)
     msg = sdk.getMsg()
     QMsg = msg.Msg
+    print(msg)
+
+    text = QMsg
+    qq = msg.QQ
+    group = msg.Group
+    timestamp = datetime.now()
+    if "pic" in text:
+        type = 'img'
+        urls = re.findall('\[IR:pic=(.+?)\]', text)
+        path = ''
+        if urls is not []:
+            for u in urls:
+                try:
+                    req = requests.get(u).content
+                    img_path = './dog_img/{QQ}_{uuid}.jpg'.format(QQ=qq, uuid=str(uuid4())[:8])
+                    path += img_path + '\n'
+                    with open(img_path, 'wb') as f:
+                        f.write(req)
+                except Exception as e:
+                    raise e
+    else:
+        type = 'text'
+        path = 'null'
+
+    message = Message(type=type, text=QMsg, path=path, qq=qq, group=group, timestamp=timestamp)
+    db.session.add(message)
+    db.session.commit()
+
     if QMsg == "来道题":
-        with open("leetcode.txt", "r", encoding="utf8") as f:
-            questions = f.read().split("\n------\n")
-            question = questions[randint(0, len(questions))]
-            sdk.sendGroupMsg(msg.Group, question)
+        pass
+        # with open("leetcode.txt", "r", encoding="utf8") as f:
+        #     questions = f.read().split("\n------\n")
+        #     question = questions[randint(0, len(questions))]
+        #     sdk.sendGroupMsg(msg.Group, question)
 
     elif "让我康康" in QMsg:
         _USERNAME = "corgiclub@yeah.net"
@@ -33,15 +81,14 @@ def index():
         illust = json_result.illusts[ri]
         id = illust.id
         title = illust.title
-        uuid_name = uuid.uuid4().hex
-        api.download(illust.image_urls[list(illust.image_urls)[-1]], path='img/', name=uuid_name + '.jpg')
+        uuid_name = uuid4().hex
+        # api.download(illust.image_urls[list(illust.image_urls)[-1]], path='img/', name=uuid_name + '.jpg')
+        api.download(illust.image_urls['large'], path='img/', name=uuid_name + '.jpg')
         address = os.getcwd() + '\\img\\' + uuid_name + '.jpg'
         print(address)
         sdk.sendGroupMsg(msg.Group, "[ksust,image:pic={}]".format(address))
         sdk.sendGroupMsg(msg.Group, "https://www.pixiv.net/member_illust.php?mode=medium&illust_id={}   "
                                     "title : {}".format(id, title))
-
-
 
     elif (QMsg.startswith("来") and QMsg.endswith("图")) or QMsg.startswith("tag") or QMsg == "帮助" or \
             QMsg.endswith("kkp") or QMsg.endswith("看看批"):
@@ -76,4 +123,6 @@ def index():
 
 
 if __name__ == '__main__':
+    # db.drop_all()
+    # db.create_all()
     app.run()
